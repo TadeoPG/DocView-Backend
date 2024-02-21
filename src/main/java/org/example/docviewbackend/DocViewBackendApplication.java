7e37cc273b500efe4fd7382d865cd6022d8c145b
TadeoPG <tadeomanuelportillo@gmail.com>
2024-02-21 01:43:30 -0500

    First commit

---
+++ src/main/java/org/example/docviewbackend/DocViewBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.docviewbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocViewBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocViewBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/docviewbackend/DocViewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.docviewbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocViewBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
