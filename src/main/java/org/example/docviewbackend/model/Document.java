327c911e6e2dae2a4ae8f57a0bdbbad4156bb06a
Yannick <yannfscz@gmail.com>
2024-02-22 00:19:17 -0500

    v1-yfunes-backend-create-document

--- src/main/java/org/example/docviewbackend/model/Document.java
+++ src/main/java/org/example/docviewbackend/model/Document.java
@@ -1,4 +1,15 @@
 package org.example.docviewbackend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // getters and setters and to string
+@AllArgsConstructor // constructor all parameters
+@NoArgsConstructor // void constructor
 public class Document {
+
+    private String URL;
+    private String title;
+    
 }
